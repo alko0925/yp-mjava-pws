36b9152c0312a50601d339d6521faab284817e22
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-11-07 15:29:53 +0300

    s3: test: Integration test implementation - DAO: H2PostRepositoryTest

--- src/test/java/ru/ya/spring3pw/integration/repository/H2PostRepositoryTest.java
+++ src/test/java/ru/ya/spring3pw/integration/repository/H2PostRepositoryTest.java
@@ -31,12 +31,57 @@ class H2PostRepositoryTest {
 
     @BeforeEach
     void setUp() {
+        jdbcTemplate.execute("DELETE FROM posts");
 
+        jdbcTemplate.update(
+                "INSERT INTO posts(id, title, text, tags) VALUES(?, ?, ?, ?)",
+                1, "Post #1", "Post Text #1", List.of().toString()
+        );
+        jdbcTemplate.update(
+                "INSERT INTO posts(id, title, text, tags) VALUES(?, ?, ?, ?)",
+                2, "Post 2", "Post Text #2", List.of().toString()
+        );
+        jdbcTemplate.update(
+                "INSERT INTO posts(id, title, text, tags) VALUES(?, ?, ?, ?)",
+                3, "Post #3", "Post Text #3", List.of().toString()
+        );
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(id, text, post_id) VALUES(?, ?, ?)",
+                1, "Comment Text #1", 3
+        );
+        jdbcTemplate.update(
+                "INSERT INTO comments(id, text, post_id) VALUES(?, ?, ?)",
+                2, "Comment Text #2", 3
+        );
     }
 
     @Test
     void getPosts_shouldReturnAllPosts() {
         List<Post> posts = postRepository.getPosts("");
+
         assertNotNull(posts);
+        assertEquals(3, posts.size());
+    }
+
+    @Test
+    void getPosts_shouldReturnJustOnePost() {
+        List<Post> posts = postRepository.getPosts("Post 2");
+
+        assertNotNull(posts);
+        assertEquals(1, posts.size());
+        Post first = posts.getFirst();
+        assertEquals(2, first.getId());
+        assertEquals("Post Text #2", first.getText());
+    }
+
+    @Test
+    void getPost_shouldReturnPostById() {
+        Post post = postRepository.getPost(1);
+
+        assertNotNull(post);
+        assertEquals(1, post.getId());
+        assertEquals("Post #1", post.getTitle());
+        assertEquals("Post Text #1", post.getText());
     }
 }
