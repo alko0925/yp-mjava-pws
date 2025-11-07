832ab4d7f4714ba59e78e210bfafc8763382d838
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-11-07 13:44:54 +0300

    s3: feat: Update sql schema with new requirements

--- src/main/resources/h2_schema.sql.sql
+++ src/main/resources/h2_schema.sql.sql
@@ -8,4 +8,34 @@ create table if not exists users(
 
 insert into users(first_name, last_name, age, active) values ('Иван', 'Иванов', 30, true);
 insert into users(first_name, last_name, age, active) values ('Пётр', 'Петров', 25, false);
-insert into users(first_name, last_name, age, active) values ('Мария', 'Сидорова', 28, true); 
+insert into users(first_name, last_name, age, active) values ('Мария', 'Сидорова', 28, true);
+
+CREATE SEQUENCE IF NOT EXISTS "seq_post_id"
+MINVALUE 1
+MAXVALUE 999999999
+INCREMENT BY 1
+START WITH 202700
+NOCACHE
+NOCYCLE;
+
+CREATE SEQUENCE IF NOT EXISTS "seq_comment_id"
+MINVALUE 1
+MAXVALUE 999999999
+INCREMENT BY 1
+START WITH 202700
+NOCACHE
+NOCYCLE;
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(400) NOT NULL,
+    text TEXT NOT NULL,
+    tags VARCHAR(400) NOT NULL,
+    likesCount INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER PRIMARY KEY,
+	post_id INTEGER REFERENCES posts ON DELETE CASCADE,
+    text VARCHAR(400) NOT NULL
+);
--- src/main/resources/postgresql_schema.sql
+++ src/main/resources/postgresql_schema.sql
@@ -8,4 +8,21 @@ create table if not exists users(
 
 insert into users(first_name, last_name, age, active) values ('Иван', 'Иванов', 30, true);
 insert into users(first_name, last_name, age, active) values ('Пётр', 'Петров', 25, false);
-insert into users(first_name, last_name, age, active) values ('Мария', 'Сидорова', 28, true); 
+insert into users(first_name, last_name, age, active) values ('Мария', 'Сидорова', 28, true);
+
+CREATE SEQUENCE IF NOT EXISTS seq_post_id AS INTEGER START 1;
+CREATE SEQUENCE IF NOT EXISTS seq_comment_id AS INTEGER START 1;
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(400) NOT NULL,
+    text TEXT NOT NULL,
+    tags VARCHAR(400) NOT NULL,
+    likesCount INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER PRIMARY KEY,
+	post_id INTEGER REFERENCES posts ON DELETE CASCADE,
+    text VARCHAR(400) NOT NULL
+);
