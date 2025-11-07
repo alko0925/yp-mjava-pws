5a6fd7313ed8db14a57d86d2ed7e6009da688c24
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-11-07 13:51:21 +0300

    s3: feat: Add Model: Post/Comment

---
+++ src/main/java/ru/ya/spring3pw/model/Comment.java
@@ -0,0 +1,41 @@
+package ru.ya.spring3pw.model;
+
+import java.util.List;
+
+public class Comment {
+    private Integer id;
+    private String text;
+    private Integer postId;
+
+    public Comment() {}
+
+    public Comment(Integer id, String text, Integer postId) {
+        this.id = id;
+        this.text = text;
+        this.postId = postId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+}
